29a362fe00596cb928d2a9ac195efc445537dc5e
fernandacaramico <fernandacaramico@gmail.com>
2021-04-28 15:20:53 -0300

    altera index de 9001

--- API-envia-dados/routes/index.js
+++ API-envia-dados/routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.status(200).send("<h1>Hello World!</h1> Para inserir dados usando a API, vá até <a href=\"http://localhost:9001/api/sendData\">http://localhost:9001/api/sendData</a>");
+  //res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
